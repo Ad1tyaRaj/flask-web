731c183b3d5ecfbb1715110d8d0d8d06bf0864da
Aditya Raj <adi78705483@gmail.com>
2024-08-17 12:29:27 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
